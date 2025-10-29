bcab56184b17884b0bc4a1c21b2ccd80de0a87b9
snakajima <satoshi.nakajima@gmail.com>
2025-10-29 10:07:02 -1000

    host: true

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
