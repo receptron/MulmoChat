dde66b77b0b406c17af9cabaa53a9df8fbf7f9d7
snakajima <satoshi.nakajima@gmail.com>
2025-10-30 04:39:50 -1000

    allow .local

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: true,
+    allowedHosts: ['.local'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
