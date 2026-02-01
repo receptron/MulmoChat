eb95733169db33afdb7a04fb2acb23674cd86d96
Masashi Honma <masashi.honma@gmail.com>
2026-02-01 19:59:51 +0900

    Fix for dynamic loading
    Provided by @isamu.
    
    Signed-off-by: Masashi Honma <masashi.honma@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ import router from "./router";
 import "./index.css";
 import "material-icons/iconfont/material-icons.css";
 
+/* Scan plugin dist files for Tailwind classes */
+import.meta.glob(
+  [
+    "../node_modules/@gui-chat-plugin/*/dist/style.css",
+    "../node_modules/@mulmochat-plugin/*/dist/style.css",
+    "../node_modules/guichat-plugin-*/dist/style.css",
+  ],
+  { eager: true },
+);
+
 createApp(App).use(router).mount("#app");
